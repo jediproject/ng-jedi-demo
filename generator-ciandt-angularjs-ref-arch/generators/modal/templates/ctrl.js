8016f2a6d9a7636305fd8373a22b8acbc41d93b7
Fábio Henrique da Silva Viana <fabiohsv@ciandt.com>
2015-07-23 23:41:35 -0300

    fix

--- generator-ciandt-angularjs-ref-arch/generators/modal/templates/ctrl.js
+++ generator-ciandt-angularjs-ref-arch/generators/modal/templates/ctrl.js
@@ -3,10 +3,10 @@
 /*
     Controller and modal for the feature <%= props.controller%>
 */
-factory.newModal('<%= props.directiveName%>', 'app/<%= props.module.toLowerCase()%>/<%if (props.submodule) {%><%= props.submodule.toLowerCase()%>/<%}%><%= props.controller.toLowerCase()%>/<%= props.controller.toLowerCase()%>.html', 'app.<%= props.module.toLowerCase()%>.<%if (props.submodule) {%><%= props.submodule.toLowerCase()%>.<%}%><%= props.controller.toLowerCase()%>.<%= props.controller.capitalize()%>Ctrl', ['<%= props.module.toLowerCase()%>RestService', <% if (props.params) {%>[<%= props.params%>], <%}%>function (<%= props.module.toLowerCase()%>RestService<% if (props.params) {%>, <%= s(props.params).replaceAll("'", '').value()%><%}%>) {
+factory.newModal('<%= props.directiveName%>', 'app/<%= props.module.toLowerCase()%>/<%if (props.submodule) {%><%= props.submodule.toLowerCase()%>/<%}%><%= props.controller.toLowerCase()%>/<%= props.controller.toLowerCase()%>.html', 'app.<%= props.module.toLowerCase()%>.<%if (props.submodule) {%><%= props.submodule.toLowerCase()%>.<%}%><%= props.controller.toLowerCase()%>.<%= props.controller.capitalize()%>Ctrl', ['yourService', <% if (props.params) {%>[<%= props.params%>], <%}%>function (yourService, <% if (props.params) {%><%= s(props.params).replaceAll("'", '').value()%><%}%>) {
 
     //#region Service initialize
-    var service = <%= props.module.toLowerCase()%>RestService.all('[yourActionBase]');
+    var service = ...//e.g: restangular instance
     //#endregion
 
     //#region View/Model initialize
