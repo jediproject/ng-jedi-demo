b50c6152c58d6d3c83a0a0344a2405dcd24b8981
Fábio Henrique da Silva Viana <fabiohsv@ciandt.com>
2015-08-26 00:34:20 -0300

    add more example components

--- app/common/features/components/components-ctrl.js
+++ app/common/features/components/components-ctrl.js
@@ -3,7 +3,7 @@
 /*
     Controller for the feature Components
 */
-jd.factory.newController('app.common.components.ComponentsCtrl', [function () {
+jd.factory.newController('app.common.components.ComponentsCtrl', ['$log', 'jedi.dialogs.AlertHelper', 'toastr', function ($log, AlertHelper, toastr) {
 
     //#region Service initialize
     var service;// = ... e.g: restangular instance
@@ -14,13 +14,19 @@ jd.factory.newController('app.common.components.ComponentsCtrl', [function () {
     vm.componentsModel = {
         list: [{
               id: 1,
-              value: 'value 1'
+              value: 'value 1',
+              date: new Date(),
+              number: 111.11
             }, {
               id: 2,
-              value: 'value 2'
+              value: 'value 2',
+              date: new Date(),
+              number: 222.22
             }, {
               id: 3,
-              value: 'value 3'
+              value: 'value 3',
+              date: new Date(),
+              number: 333.33
             }
         ]
     };
@@ -29,6 +35,10 @@ jd.factory.newController('app.common.components.ComponentsCtrl', [function () {
     //#region Events binds
     vm.method1 = method1;
     vm.method2 = method2;
+    vm.method3 = method3;
+    vm.method4 = method4;
+    vm.method5 = method5;
+    vm.method6 = method6;
     //#endregion
 
     //#region Load controller
@@ -37,15 +47,37 @@ jd.factory.newController('app.common.components.ComponentsCtrl', [function () {
 
     //#region Events definitions
     function method1() {
-        console.log(vm.componentsModel);
+        $log.info(vm.componentsModel);
     }
 
     function method2() {
-        console.log(vm.componentsModel);
+        $log.info(vm.componentsModel);
         vm.componentsModel.list.push({
             id: vm.componentsModel.list.length+1,
-            value: 'value value value value value '+(vm.componentsModel.list.length+1)
+            value: 'value value value value value '+(vm.componentsModel.list.length+1),
+            date: new Date(),
+            number: vm.componentsModel.list.length+1
         });
+    }
+
+    function method3() {
+        $log.info(vm.componentsModel);
+        AlertHelper.addInfo('success');
+    }
+
+    function method4() {
+        $log.info(vm.componentsModel);
+        AlertHelper.addError('error');
+    }
+
+    function method5() {
+        $log.info(vm.componentsModel);
+        toastr.success('success');
+    }
+
+    function method6() {
+        $log.info(vm.componentsModel);
+        toastr.error('error');
     }
     //#endregion
 
