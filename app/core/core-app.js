6b99f6608ed22bdad61730089f3cebdd2cf9c574
Fábio Henrique da Silva Viana <fabiohsv@ciandt.com>
2015-10-08 11:37:38 -0300

    add config and run blocks

--- app/core/core-app.js
+++ app/core/core-app.js
@@ -3,5 +3,12 @@
 jd.factory.newModule('core', {
     internalDeps: [
         // insert shared deps into the module, e.g.: directives, filters, controllers...
-    ]
+    ],
+    config: ['$httpProvider', 'jedi.utilities.UtilitiesProvider', function($httpProvider, Utilities) {
+    	var $log = angular.injector(['ng']).get('$log');
+    	$log.info('Test config module');
+    }],
+    run: ['$log', 'jedi.utilities.Utilities', function($log, Utilities) {
+    	$log.info('Test run module');
+    }]
 });
