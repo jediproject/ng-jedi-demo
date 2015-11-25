482784df2e651116efcc06e1963ced3e5f0e7514
Tanato <tanato@ciandt.com>
2015-11-25 17:58:07 -0200

    Configuração inicial Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -15,48 +15,26 @@ var isProduction = argv.env === 'master' || argv.env === 'mirror';
 
 gulp.task('default', function () {
     // place code for your default task here
+    
 });
 
 gulp.task('clean', function () {
-    return gulp.src('build', 'assets', '!assets/css/app.css', '!assets/img')
+    return gulp.src('build')
         .pipe(clean());
 })
 
-gulp.task('assets', ['clean'], function () {
+gulp.task('assets', function () {
     var libs = gulp.src(assets.libs.src, { base: 'bower_components/' })
         .pipe(flatten({ includeParents: 1 }))
-        .pipe(gulp.dest(assets.libs.dest));
-
-    var css = gulp.src(assets.css.src)
-        .pipe(gulp.dest(assets.css.dest));
-
-    var fonts = gulp.src(assets.fonts.src)
-        .pipe(gulp.dest(assets.fonts.dest));
-
-    return merge(libs, css, fonts);
-});
-
-gulp.task('copy', function() {
-    return gulp.src(['**/*.*', '!**/*.js', '!**/*.css', '!**/*.tpl.*', '!**/env/*.json', '**/env/*-env.json'])
-        .pipe(gulp.dest('folder'));
-});
-
-gulp.task('build', function () {
-    var filterJSMin = gulpFilter(['**/*', '!.min.*'], { restore: true });
-    var libs = gulp.src(assets.libs.src, { base: 'bower_components/' })
         .pipe(gulpif(isProduction, uglify()))
-        .pipe(filterJSMin)
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(filterJSMin.restore)
-        .pipe(flatten({ includeParents: 1 }))
         .pipe(gulp.dest("build/" + assets.libs.dest));
 
-    var filterCssMin = gulpFilter(['**/*', '!.min.*'], { restore: true });
+    var filterMin = gulpFilter(['*', '!.min.*'], { restore: true });
     var css = gulp.src(assets.css.src)
         .pipe(gulpif(isProduction, minifyCss()))
-        .pipe(filterCssMin)
+        .pipe(filterMin)
         .pipe(rename({ suffix: '.min' }))
-        .pipe(filterCssMin.restore)
+        .pipe(filterMin.restore)
         .pipe(gulp.dest("build/" + assets.css.dest));
 
     var fonts = gulp.src(assets.fonts.src)
