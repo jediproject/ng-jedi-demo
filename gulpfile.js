e65a1cb213d651e7700534bb6619ad2ab73d9e35
Tanato Cartaxo <tanato@ciandt.com>
2015-12-03 19:23:36 -0200

    Partial commit of Gulp Tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,29 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     gulpif = require('gulp-if'),
     minifyCss = require('gulp-minify-css'),
-    gulpFilter = require('gulp-filter'),
+    filter = require('gulp-filter'),
     rename = require('gulp-rename');
 
 var isProduction = argv.env === 'master' || argv.env === 'mirror';
 
 
-gulp.task('default', function () {
-    // place code for your default task here
-    
+gulp.task('default', ['assets'], function () {
+    var a = 1;// place code for your default task here
 });
 
+// Clean Build
 gulp.task('clean', function () {
     return gulp.src(['assets/libs', 'assets/css/*', 'assets/fonts', '!assets/css/app.css'])
         .pipe(clean());
 })
 
+// Clean Build
+gulp.task('cleanbuild', function () {
+    return gulp.src(['build/*'])
+        .pipe(clean());
+})
+
+// Copy Assets
 gulp.task('assets', ['clean'], function () {
     var libs = gulp.src(assets.libs.src, { base: 'bower_components/' })
         .pipe(flatten({ includeParents: 1 }))
@@ -37,31 +44,46 @@ gulp.task('assets', ['clean'], function () {
     return merge(libs, css, fonts);
 });
 
-gulp.task('copy', function() {
-    return gulp.src(['**/*.*', '!**/*.js', '!**/*.css', '!**/*.tpl.*', '!**/env/*.json', '**/env/*-env.json'])
-        .pipe(gulp.dest('folder'));
-});
+// Run Build / Buildmin
+gulp.task('build', ['cleanbuild', 'assets'], function () {
 
-gulp.task('build', function () {
-    var filterJSMin = gulpFilter(['**/*', '!.min.*'], { restore: true });
-    var libs = gulp.src(assets.libs.src, { base: 'bower_components/' })
-        .pipe(gulpif(isProduction, uglify()))
-        .pipe(filterJSMin)
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(filterJSMin.restore)
-        .pipe(flatten({ includeParents: 1 }))
-        .pipe(gulp.dest("build/" + assets.libs.dest));
+    var app = gulp.src(['**/*.*', '!**/*.tpl.*', '!**/env/*.json', '**/env/*-env.json'], { cwd: 'app/' })
+    if (isProduction) {
+        var filterAppCss = filter(['**/*.css', '!.min.*'], { restore: true });
+        app.pipe(filterAppCss)
+            .pipe(minifyCss())
+            .pipe(rename({ suffix: '.min' }))
+            .pipe(filterAppCss.restore);
 
-    var filterCssMin = gulpFilter(['**/*', '!.min.*'], { restore: true });
-    var css = gulp.src(assets.css.src)
-        .pipe(gulpif(isProduction, minifyCss()))
-        .pipe(filterCssMin)
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(filterCssMin.restore)
-        .pipe(gulp.dest("build/" + assets.css.dest));
+        var filterAppJs = filter(['**/*.js', '!.min.*'], { restore: true });
+        app.pipe(filterAppJs)
+            .pipe(uglify())
+            .pipe(rename({ suffix: '.min' }))
+            .pipe(filterAppJs.restore);
+    }
+    app.pipe(gulp.dest('build/app/'));
 
-    var fonts = gulp.src(assets.fonts.src)
-        .pipe(gulp.dest("build/" + assets.fonts.dest));
+    var assets = gulp.src(['**/*.*'], { cwd: 'assets/' })
+    if (isProduction) {
+        var filterAssetsCss = filter(['**/*.css', '!.min.*'], { restore: true });
+        assets.pipe(filterAssetsCss)
+            .pipe(minifyCss())
+            .pipe(rename({ suffix: '.min' }))
+            .pipe(filterAssetsCss.restore)
 
-    return merge(libs, css, fonts);
+        var filterAssetsJs = filter(['**/*.js', '!.min.*'], { restore: true });
+        assets.pipe(filterAssetsJs)
+            .pipe(uglify())
+            .pipe(rename({ suffix: '.min' }))
+            .pipe(filterAssetsJs.restore)
+    }
+    assets.pipe(gulp.dest('build/assets/'));
+
+    var build = gulp.src(['favicon.ico', 'index.html', 'main.tpl.js', 'version.tpl.json'])
+        .pipe(rename(function (path) {
+            path.basename = path.basename.replace('.tpl', '');
+        }))
+        .pipe(gulp.dest('build/'));
+
+    return merge(app, assets, build);
 });
