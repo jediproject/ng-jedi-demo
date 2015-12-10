42055e8cacaf3875877585c96eb26fb76b694421
Tanato Cartaxo <tanato@ciandt.com>
2015-12-10 15:23:21 -0200

    Added Set Environment Task

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,18 @@ var gulp = require('gulp'),
     clean = require('gulp-clean'),
     uglify = require('gulp-uglify'),
     gulpif = require('gulp-if'),
+    gutil = require('gulp-util'),
     minifyCss = require('gulp-minify-css'),
-    gulpFilter = require('gulp-filter'),
-    rename = require('gulp-rename');
+    filter = require('gulp-filter'),
+    rename = require('gulp-rename'),
+    replaceTask = require('gulp-replace-task'),
+    lazypipe = require('lazypipe'),
+    rev = require('gulp-rev'),
+    revReplace = require('gulp-rev-replace'),
+    jshint = require('gulp-jshint');
+
+var packageJSON = require('./package');
+var jshintConfig = packageJSON.jshintConfig;
 
 var isProduction = argv.env === 'master' || argv.env === 'mirror';
 
@@ -17,7 +26,6 @@ gulp.task('default', function () {
     // place code for your default task here
 
 });
-
 gulp.task('clean', function () {
     return gulp.src('build')
         .pipe(clean());
@@ -42,3 +50,36 @@ gulp.task('assets', function () {
 
     return merge(libs, css, fonts);
 });
+
+gulp.task('setEnvironment', function () {
+    if (!argv.env) {
+        gutil.log(gutil.colors.bold.red('[ERROR:] A environment parameter like "--env=master" is needed for the setEnvironment task'));
+        return;
+    }
+    
+    // Get modules from folders inside "./app/"
+    var modules = getModules();
+
+    //Set environment for each module
+    for (var i = 0; i < Object.keys(modules).length; i++) {
+
+        // Read Environment specific Settings
+        var jsonEnv = require('./app/' + modules[i] + '/env/' + modules[i] + '-env.' + argv.env + '.json');
+
+        // Replace Settings in template
+        gulp.src('app/' + modules[i] + '/env/' + modules[i] + '-env.tpl.json')
+            .pipe(replaceTask({
+                patterns: [{ json: jsonEnv }]
+            }))
+            .pipe(rename(modules[i] + '-env.json'))
+            .pipe(gulp.dest('app/' + modules[i] + '/env/'));
+    }
+});
+
+// Get modules from directories inside ./app/ folder
+function getModules() {
+    var fs = require('fs'), path = require('path');
+    return fs.readdirSync('./app').filter(function (file) {
+        return fs.statSync(path.join('./app', file)).isDirectory();
+    });
+}
