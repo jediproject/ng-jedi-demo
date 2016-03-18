5c251e268932446ce26a76d7e814be37482a7d80
Fábio Henrique da Silva Viana <fabiohsv@ciandt.com>
2016-03-18 17:29:58 -0300

    change ng-currency-mask and ngMask to angular-input-masks

--- app/common/common-app.js
+++ app/common/common-app.js
@@ -1,18 +1,34 @@
 "use strict";
 
-define(['ng-currency-mask',
-        'angular-ngMask',
+define(['angular-input-masks',
         'angular-bootstrap',
-        'angular-file-upload',        
-        'app-common-components',
-        'angular-toastr'], function () {
-    
-    angular.module("app.external.components", [ 'ngCurrencyMask',
-                                                'ngMask',
+        'angular-file-upload',
+        'angular-toastr',
+        'ng-jedi-i18n',
+        'ng-jedi-dialogs',
+        'ng-jedi-factory',
+        'ng-jedi-loading',
+        'ng-jedi-breadcrumb',
+        'ng-jedi-layout',
+        'ng-jedi-security',
+        'ng-jedi-table',
+        'ng-jedi-activities',        
+        'app-common-components'], function () {
+
+    angular.module("app.external.components", [ 'ui.utils.masks',
                                                 'ui.bootstrap',
                                                 'angularFileUpload',                                                
-                                                'toastr' ]);
+                                                'toastr',
+                                                'jedi.i18n',
+                                                'jedi.dialogs',
+                                                'jedi.factory',
+                                                'jedi.loading',
+                                                'jedi.breadcrumb',
+                                                'jedi.layout',
+                                                'jedi.security',
+                                                'jedi.table',
+                                                'jedi.activities' ]);
 
     angular.module("app.common", ['app.external.components', 'app.common.components']);
-    
+
 });
--- app/common/components/components.js
+++ app/common/components/components.js
@@ -1,25 +1,7 @@
 'use strict';
 
-define(['ng-jedi-i18n',
-        'ng-jedi-dialogs',
-        'ng-jedi-factory',
-        'ng-jedi-loading',
-        'ng-jedi-breadcrumb',
-        'ng-jedi-layout',
-        'ng-jedi-security',
-        'ng-jedi-table',
-        'ng-jedi-activities',
-        'app-common-components-exceptions'], function () {
+define(['app-common-components-exceptions'], function () {
 
-    angular.module('app.common.components', ['jedi.i18n',
-                                             'jedi.dialogs',
-                                             'jedi.factory',
-                                             'jedi.loading',
-                                             'jedi.breadcrumb',
-                                             'jedi.layout',
-                                             'jedi.security',
-                                             'jedi.table',
-                                             'jedi.activities',
-                                             'app.common.components.exceptions']);
+    angular.module('app.common.components', ['app.common.components.exceptions']);
 
 });
